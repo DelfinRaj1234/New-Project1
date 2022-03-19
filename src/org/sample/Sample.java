3ed5536c509a6baf76d6a99ce98eda672ad69403
DelfinRaj1234 <delfinraj2022@gmail.com>
2022-03-19 19:04:05 +0530

    project added

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,15 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		 int no = 123;
+			 int sum = 0;
+			 int rem = 0;
+			 while (no > 0) {
+				rem = no % 10;
+				sum = sum + rem;
+				no = no / 10;
+			}
+			 System.out.println(sum);
+	}
+}
